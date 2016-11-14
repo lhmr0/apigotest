418a3e857401c67937e4d00b6dee972ab2702c6d
Luis <luchiitoz@gmail.com>
2016-11-13 20:27:18 -0500

    depencias

--- restapi.go
+++ restapi.go
@@ -65,5 +65,5 @@ func main() {
     router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
     router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
     router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
- log.Fatal(http.ListenAndServe(":12345", router))
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
